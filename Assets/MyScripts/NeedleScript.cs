4c640d43434cc551a7a36e71150b488381fb5aca
Nate Mendoza <nmendoza82rg@aol.com>
2018-10-20 11:25:35 -0700

    1.10
    Working on the music player part. Got the play and stop buttons to work now need to get the pause button to stop the needle in its current position.
    next step to ask how to save the number sequences

---
+++ Assets/MyScripts/NeedleScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleScript : MonoBehaviour {
+
+
+    public float needleMoveSpeed;
+    public Transform startingPos;
+    public enum State {Stop = 0, Pause = 1, Play = 2}
+    public State needlesState;
+    Rigidbody2D needleBody;
+
+    
+    void Update()
+    {
+        if (needlesState == State.Pause)
+            {
+                NeedlePause();
+            }
+        if (needlesState == State.Play)
+        {
+            NeedlePlay();
+        
+            
+        }
+        else
+            NeedleStop();
+        
+    }
+    private void Start()
+    {
+        needleBody = GetComponent<Rigidbody2D>();
+        //needlesState = State.Stop;
+    }
+
+
+    public void PlayButtonPress()
+    {
+        needlesState = State.Play;
+    }
+
+    public void PauseButtonPress()
+    {
+        needlesState = State.Pause;
+    }
+
+    public void StopButtonPress()
+    {
+        needlesState = State.Stop;
+    }
+
+    public void NeedlePlay()
+    {
+        
+        float playBackSpeed = needleMoveSpeed * Time.deltaTime;
+
+        transform.Translate(Vector2.right * playBackSpeed);
+
+        //if (tranform.position >= )
+    }
+
+    public void NeedlePause()
+    {
+
+
+        needleBody.constraints = RigidbodyConstraints2D.FreezePosition;
+    }
+
+    public void NeedleStop()
+    {
+        transform.position = startingPos.position;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D contact)
+    {
+        if (contact.gameObject.tag == "End")
+        {
+            transform.position = startingPos.position;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D contact)
+    {
+
+        Debug.Log("I am touching" + contact.gameObject.name);
+        
+    }
+
+}
+
+
+
